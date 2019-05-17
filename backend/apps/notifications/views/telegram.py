0ea39b8109dc0208eb44639f2042c846aa0a7115
Herlan Assis <herlan@evocorp.com.br>
2019-05-17 10:18:38 -0300

    TEstando bot

--- backend/apps/notifications/views/telegram.py
+++ backend/apps/notifications/views/telegram.py
@@ -8,13 +8,14 @@ from rest_framework.views import APIView
 from rest_framework.utils import encoders
 from django.core.cache import cache
 from django.conf import settings
-import logging
 import telegram
 from telegram.error import NetworkError, Unauthorized
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
                           ConversationHandler)
 from time import sleep
+import telepot
+from telepot import loop, Bot
 from ..models import Watcher
 
 
@@ -31,65 +32,117 @@ class MeninoDeRecadoBot(TelegramAPIView):
     return Response("")
 
 # Telegram Bot Authorization Token
-bot = telegram.Bot('722189281:AAEm1AEGB5XeXjUrFBru8zB8ND-wTHoelrE')
+# bot = telegram.Bot(os.environ.get("TELEGRAM_BOT_TOKEN"))
+
+projetos_monitorados="/projetos_monitorados"
+monitorar_novo_projeto="/monitorar_projeto"
+cancelar_monitoramento="/cancelar_monitoramento"
 # Comandos
-projetos_monitorados="Projetos monitorados"
-monitorar_novo_projeto="Monitorar um novo projeto"
-cancelar_monitoramento="Cancelar monitoramente"
-reply_keyboard = [[projetos_monitorados], [monitorar_novo_projeto], [cancelar_monitoramento]]
+comando_projetos_monitorados="{} para listar os projetos monitorados.\n".format(projetos_monitorados)
+comando_monitorar_novo_projeto="{} seguido do código de autorização para monitorar um novo projeto.\n".format(monitorar_novo_projeto)
+comando_cancelar_monitoramento="{} seguido do código de autorização para cancelar um monitoramento.\n".format(cancelar_monitoramento)
+# reply_keyboard = [[projetos_monitorados], [monitorar_novo_projeto], [cancelar_monitoramento]]
 
 def start_or_help(update):
   update.message.reply_text(
     'Oi! Eu sou responsável por notificar pessoas.\n',
-    'O que você deseja fazer?',
-    reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    'O que você deseja fazer?\n',
+    comando_projetos_monitorados,
+    comando_monitorar_novo_projeto,
+    comando_cancelar_monitoramento,
+  )
 
 def listar_projetos(update):
   meus_monitoramentos=Watcher.objects.all().filter(telegram_chat_id=update.message.chat.id)
-  print(meus_monitoramentos)
+  if(meus_monitoramentos):    
+    update.message.reply_text(
+      '{}'.format('\n'.join(map(str, meus_monitoramentos))),
+    )
+  else:  
+    update.message.reply_text(
+      'Você não ten nenhum projeto monitorado!',
+    )
 
-def monitorar_projeto(update):
-  projeto=Watcher.objects.get(authorization_code=update.message)
+def monitorar_projeto(update, code):
+  projeto=Watcher.objects.get(authorization_code=code)
   
   if projeto:
     projeto.telegram_chat_id=update.message.chat.id
+    projeto.reset_authorization_code()
+    projeto.save()    
+    update.message.reply_text(
+      'Projeto monitorado!',
+    )
+  else:
+    update.message.reply_text(
+      'Não entendi, caso tenha dúvidas tente utilizar o /help.',
+    )
+
+def cancelar_monitoramento(update, code):
+  projeto=Watcher.objects.get(authorization_code=code)
+  
+  if projeto:
+    projeto.telegram_chat_id=''
+    projeto.reset_authorization_code()
     projeto.save()
+    update.message.reply_text(
+      'Monitoramento cancelado!',
+    )
+  else:
+    update.message.reply_text(
+      'Não entendi, caso tenha dúvidas tente utilizar o /help.',
+    )
 
-  print(projeto)
+# def handler(bot):
+#   global update_id
+#   # Request updates after the last update_id
+#   for update in bot.get_updates(offset=update_id, timeout=10):
+#       update_id = update.update_id + 1
+#       print(update)
 
-def handler(bot):
-  global update_id
-  # Request updates after the last update_id
-  for update in bot.get_updates(offset=update_id, timeout=10):
-      update_id = update.update_id + 1
-      print(update)
+#       if update.message:  # your bot can receive updates without messages
+#         text = update.message.text
+#         command = text.split(' ')          
 
-      if update.message:  # your bot can receive updates without messages
-        if update.message.text=='/start' or update.message.text=='/help':
-          start_or_help(update)
-        if update.message.text == projetos_monitorados:
-          listar_projetos(update)
-        if update.message.text == monitorar_novo_projeto:
-          pass
-        if update.message.text == cancelar_monitoramento:
-          pass
-        else:
-          update.message.reply_text(
-            'Não entendi, caso tenha dúvidas tente utilizar o /help.',
-          )
+#         if text=='/start' or text=='/help':
+#           start_or_help(update)
+#         if projetos_monitorados in text:
+#           listar_projetos(update)
+#         if monitorar_novo_projeto in text:
+#           if len(command) > 1:
+#             monitorar_novo_projeto(update, command[1])          
+#         if cancelar_monitoramento in text:
+#           if len(command) > 1:
+#             cancelar_monitoramento            
+#         else:
+#           update.message.reply_text(
+#             'Não entendi, caso tenha dúvidas tente utilizar o /help.',
+#           )
 
-try:
-  update_id = bot.get_updates()[0].update_id
-except IndexError:
-  update_id = None
+# try:
+#   update_id = bot.get_updates()[0].update_id
+# except IndexError:
+#   update_id = None
 
-while True:
-  try:
-      handler(bot)
-  except NetworkError:
-      sleep(1)
-  except Unauthorized:
-      # The user has removed or blocked the bot.
-      update_id += 1
+# while True:
+#   try:
+#       handler(bot)
+#   except NetworkError:
+#       sleep(1)
+#   except Unauthorized:
+#       # The user has removed or blocked the bot.
+#       update_id += 1
 
 
+def handle(msg):
+  content_type, chat_type, chat_id = telepot.glance(msg)
+  print(msg)
+
+  if content_type == 'text':
+    bot.sendMessage(chat_id, msg['text'])
+
+
+bot = Bot(os.environ.get("TELEGRAM_BOT_TOKEN"))
+bot.setWebhook("http://35.199.110.204/{}".format("api/n/telegrambot/"), max_connections=1)
+# webhook = loop.OrderedWebhook(bot, handle)
+# webhook.run_as_thread()
