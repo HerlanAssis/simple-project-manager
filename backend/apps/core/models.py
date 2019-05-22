c8ff07d660e317a04bd72eddd0196361579a2575
Herlan Assis <herlan@evocorp.com.br>
2019-05-22 19:53:34 -0300

    Change base model

--- backend/apps/core/models.py
+++ backend/apps/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class BaseModel(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+  enable = models.BooleanField(default=True)
 
   class Meta:
     abstract=True
