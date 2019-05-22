18efb601f6617a39b5ecc80eda949f891733fb0c
Herlan Assis <herlan@evocorp.com.br>
2019-05-22 20:23:09 -0300

    Add security verification

--- backend/apps/notifications/schema/mutation.py
+++ backend/apps/notifications/schema/mutation.py
@@ -1,8 +1,73 @@
 import graphene
+from github import Github
 from graphene_django.types import DjangoObjectType
 from ..models import Watcher, History
 from .types import WatcherType
 from .inputs import WatcherInput
+
+class CreateWatcherAsGuest(graphene.Mutation):
+  class Arguments:
+    invitation_code=graphene.String(required=True)    
+
+  ok = graphene.Boolean()
+  watcher = graphene.Field(WatcherType)
+
+  @staticmethod
+  def mutate(root, info, invitation_code, input=None):
+    ok = False
+    vigilant_instance = Watcher.getVigilantBy(invitation_code=invitation_code)
+
+    if vigilant_instance:
+      # invalidate invitation code
+      vigilant_instance.resetInvitationCode()
+      
+      ok = True
+      watcher_instance = Watcher(
+        observer=info.context.user,      
+        vigilant=vigilant_instance,
+      )
+
+      watcher_instance.save()
+      return CreateWatcher(ok=ok, watcher=watcher_instance)
+    return CreateWatcher(ok=ok, watcher=None)
+
+class CreateWatcherAsContributor(graphene.Mutation):
+  class Arguments:    
+    project_id=graphene.String(required=True)
+    reponame=graphene.String(required=True)
+
+  ok = graphene.Boolean()
+  watcher = graphene.Field(WatcherType)
+
+  @staticmethod
+  def mutate(root, info, project_id, input=None):
+    ok = False
+
+    # verificar se ele é um contribuidor
+    try:
+      access_token = info.context.user.social_auth.get(provider='github').extra_data['access_token']
+      github_instance = Github(login_or_token=access_token)
+      repo = github_instance.get_user().get_repo(reponame)
+
+      if repo and repo.id =! project_id: ## esse malandro não participa desse projeto
+        return CreateWatcher(ok=ok, watcher=None)
+    except Exception as e:
+      return CreateWatcher(ok=ok, watcher=None)
+
+    #segue o fluxo...
+
+    vigilant_instance = Watcher.getVigilantBy(project_id=project_id)
+
+    if vigilant_instance:      
+      ok = True
+      watcher_instance = Watcher(
+        observer=info.context.user,      
+        vigilant=vigilant_instance,
+      )
+
+      watcher_instance.save()
+      return CreateWatcher(ok=ok, watcher=watcher_instance)
+    return CreateWatcher(ok=ok, watcher=None)
 
 class CreateWatcher(graphene.Mutation):
   class Arguments:
@@ -53,5 +118,6 @@ class UpdateWatcher(graphene.Mutation):
 
 
 class Mutation(graphene.ObjectType):
-  create_Watcher = CreateWatcher.Field()
+  create_Watcher_as_contributor = CreateWatcherAsContributor.Field()
+  create_Watcher_as_guest = CreateWatcherAsGuest.Field()
   update_Watcher = UpdateWatcher.Field()
