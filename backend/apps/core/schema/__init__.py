2a1b4daf17cf58e5836fc0d6ec75bcf341b80800
Herlan Assis <herlan@evocorp.com.br>
2019-05-29 20:37:35 -0300

    Some changes

---
+++ backend/apps/core/schema/__init__.py
@@ -0,0 +1,17 @@
+import graphene
+#import apps.project_manager.schema
+from apps.tasks.schema.query import Query as TasksQuery
+from apps.notifications.schema.query import Query as NotificationsQuery
+
+from apps.tasks.schema.mutation import Mutation as TasksMutation
+from apps.notifications.schema.mutation import Mutation as NotificationsMutation
+
+class Query(TasksQuery, NotificationsQuery):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
+
+class Mutation(TasksMutation, NotificationsMutation):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    pass
