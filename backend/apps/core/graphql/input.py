076209ea8f28a274093b04dd4d1761912775d05b
Herlan Assis <herlan@evocorp.com.br>
2019-05-29 20:56:59 -0300

    Correcao

--- backend/apps/core/graphql/input.py
+++ backend/apps/core/graphql/input.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django.types import DjangoObjectType
 
-class User(graphene.InputObjectType):
+class UserInput(graphene.InputObjectType):
   id = graphene.ID()
   username = graphene.String()
   email = graphene.String()
