f59712a36e47bfae5e4b104a2008fb5f3758d7ae
Herlan Assis <herlan@evocorp.com.br>
2019-06-13 17:58:28 -0300

    Changes

--- backend/apps/reports/views/pdf.py
+++ backend/apps/reports/views/pdf.py
@@ -6,61 +6,92 @@ from rest_framework.settings import api_settings
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from weasyprint import HTML
+from weasyprint import HTML, CSS
 
 from apps.tasks.models import TaskManager
 
-def convertTaskManagerArrayToPDFReportFormat(taskmanagers):
+def convertTaskManagersToDetailReport(taskmanagers):
   lines = []
-  cell_columns_names = ['Tarefas atrasadas', 'Tarefas bloquedas', 'Tarefas completas', 'Tarefas completas atrasadas', 'Tarefas abertas', 'Total', 'Progresso']
+  
+  cell_columns_names = [
+    'Projeto',
+    'Tarefas atrasadas',
+    'Tarefas bloquedas',
+    'Tarefas completas atrasadas',
+    'Tarefas completas',
+    'Tarefas abertas',
+    'Total'
+  ]
 
   lines.append(cell_columns_names)
 
   for taskmanager in taskmanagers:
     cell = []
+    cell.append(taskmanager.project_name)
     cell.append(taskmanager.qtd_overdue_tasks)
-    cell.append(taskmanager.qtd_overdue_tasks)
-    cell.append(taskmanager.qtd_overdue_tasks)
-    cell.append(taskmanager.qtd_overdue_tasks)
-    cell.append(taskmanager.qtd_overdue_tasks)
+    cell.append(taskmanager.qtd_blocked_tasks)
+    cell.append(taskmanager.qtd_tasks_completed_late)
+    cell.append(taskmanager.qtd_completed_tasks)
+    cell.append(taskmanager.qtd_open_tasks)
     cell.append(taskmanager.qtd_tasks)
-    cell.append(taskmanager.progress)
     
     lines.append(cell)
-
   return lines
+
+
+class ORIENTATIONS:
+  LANDSCAPE = 'landscape'
+  PORTRAYED = 'A4'
 
 class ReportAPIView(APIView):
     authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES
-    permission_classes = (IsAuthenticated,)   
+    permission_classes = (IsAuthenticated,)
+
+    def response_file(self, filename='report.pdf', title='Report', data=[], template_name='', orientation=ORIENTATIONS.LANDSCAPE):
+      path = '/tmp'
+      fullpath = '{}/{}'.format(path, filename)
+      template_base = 'reports'
+      template = '{}/{}'.format(template_base, template_name)
+      context = {
+        'title': title,
+        'lines': data
+      }
+
+      html_string = render_to_string(template, context)
+      html = HTML(string=html_string)
+      style = '@page {}{}{}'.format('{', 'size: {}; margin: 0.5cm'.format(orientation) , '}')
+
+      css = CSS(string=style)
+      html.write_pdf(target=fullpath, stylesheets=[css])
+
+      fs = FileSystemStorage(path)
+      with fs.open(filename) as pdf:
+          response = HttpResponse(pdf, content_type='application/pdf')
+          response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+          return response
+
+      return response
 
 
 class DetailReportByTaskManager(ReportAPIView):
-  def get(self, request, id, format=None):
-    path='/tmp'
-    filename='{}.pdf'.format(request.user.username)
-    fullpath='{}/{}'.format(path, filename)
-    
+  def get(self, request, id, format=None):    
+    return HttpResponse('')
 
-    taskmanagers = TaskManager.objects.all()
-    lines = convertTaskManagerArrayToPDFReportFormat(taskmanagers)
-
-    print(lines)
-
-    paragraphs = ['first paragraph', 'second paragraph', 'third paragraph']
-    html_string = render_to_string('reports/detail_report_by_task_manager.html', {'title': 'TESTE', 'lines': lines})
-
-    html = HTML(string=html_string)
-    html.write_pdf(target=fullpath)
-
-    fs = FileSystemStorage(path)
-    with fs.open(filename) as pdf:
-        response = HttpResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
-        return response
-
-    return response
 
 class ResumeReportByTaskManager(ReportAPIView):
   def get(self, request, id, format=None):
-    return HttpResponse('')
+
+    filename = '{}.pdf'.format(request.user.username)
+    title = 'Relatório resumido de projetos'
+    data = convertTaskManagersToDetailReport(TaskManager.objects.all())
+    template_name = 'detail_report_by_task_manager.html'
+
+    response = self.response_file(
+      filename=filename,
+      title=title,
+      data=data,
+      template_name=template_name,
+      orientation=ORIENTATIONS.LANDSCAPE
+    )
+
+    return response
