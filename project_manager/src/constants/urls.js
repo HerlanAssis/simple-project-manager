dc9eca08dfe56691088b5a143a756e2405af498a
Herlan Assis <herlan@evocorp.com.br>
2019-06-21 13:28:20 -0300

    Mudancas no proxy

--- project_manager/src/constants/urls.js
+++ project_manager/src/constants/urls.js
@@ -2,8 +2,10 @@
 // export const API_BASE_URL = 'http://localhost:8080/api';
 // export const GITHUB_REDIRECT_URL = 'http://localhost/login';
 // PROD
-export const API_BASE_URL = 'http://simpleprojectmanager.site:8080/api';
-export const GITHUB_REDIRECT_URL = 'http://simpleprojectmanager.site/login';
+export const API_BASE_URL = 'http://server.simpleprojectmanager.site:8080/api';
+
+export const GITHUB_REDIRECT_URL = 'http://manager.simpleprojectmanager.site/login';
 export const GITHUB_AUTHORIZE = 'https://github.com/login/oauth/authorize?';
-export const GITHUB_REPO_URL = 'https://github.com/HerlanAssis/simple-project-manager';
+
 export const TELEGRAM_BOT_URL = 'https://telegram.me/MeninoDeRecado';
+export const GITHUB_REPO_URL = 'https://github.com/HerlanAssis/simple-project-manager';
