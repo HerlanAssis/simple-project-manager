6b17050a7559bf9ec1892c320ceb41c2db53c27e
herlanassis <herlanassis@gmail.com>
2019-06-28 14:45:39 +0000

    Fix urls

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -3,9 +3,9 @@
 // export const GOOGLE_REDIRECT_URL = 'http://spm.com.br:3000/login';
 
 // Prod
-export const API_BASE_URL = 'http://server.simpleprojectmanager.site:8080/api';
+export const API_BASE_URL = 'http://server.simpleprojectmanager.site/api';
 
-export const GOOGLE_REDIRECT_URL = 'http://client.simpleprojectmanager.site:3000/login';
+export const GOOGLE_REDIRECT_URL = 'http://client.simpleprojectmanager.site/login';
 export const GOOGLE_AUTHORIZE = 'https://accounts.google.com/o/oauth2/v2/auth?';
 
 export const TELEGRAM_BOT_URL = 'https://telegram.me/MeninoDeRecado';
